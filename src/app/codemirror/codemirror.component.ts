2cbf67d940967738686d7937da29abf73be76dd2
mosarpide <mosapride01@gmail.com>
2018-07-21 22:10:58 +0900

    fix : ctrl+f add

--- src/app/codemirror/codemirror.component.ts
+++ src/app/codemirror/codemirror.component.ts
@@ -39,7 +39,7 @@ export class CodemirrorComponent implements AfterContentInit, OnDestroy {
       lineWrapping: true,
       value: this.markdown,
       theme: 'default',
-      extraKeys: { 'Enter': 'newlineAndIndentContinueMarkdownList' },
+      extraKeys: { 'Enter': 'newlineAndIndentContinueMarkdownList', 'Ctrl-F': 'findPersistent', 'Cmd-F': 'findPersistent' },
     });
 
 
